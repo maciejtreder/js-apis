1de596e2c09cecc8c856032d2fea6eceed15ffb3
Maciej Treder <contact@maciejtreder.com>
2019-12-06 22:01:11 +0100

    menu reorganization

--- geo_localization/geo_localization.js
+++ geo_localization/geo_localization.js
@@ -2,17 +2,12 @@
 $(document).ready(() => {
    const details = document.getElementById("details");
    if ("geolocation" in navigator) {
-
       navigator.geolocation.getCurrentPosition((position) => {
-
-         console.log(position);
-
          details.innerHTML = position.coords.latitude + ',' + position.coords.longitude;
-
       });
-    } else {
+   } else {
       details.innerHTML = 'This browser does not support geo localization API';
-    }
+   }
 
 });
 
