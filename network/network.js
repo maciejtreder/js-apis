8578d0c78a2ea2614d6787704fff73ce322bc0b8
Maciej Treder <contact@maciejtreder.com>
2019-12-06 22:08:34 +0100

    network

---
+++ network/network.js
@@ -0,0 +1,21 @@
+function getConnection() {
+  return navigator.connection || navigator.mozConnection ||
+  navigator.webkitConnection || navigator.msConnection;
+}
+
+$(document).ready(() => {
+
+   window.addEventListener('online', () => $('#online').text('online'));
+   window.addEventListener('offline', () => $('#online').text('offline'));
+
+   const info = getConnection();
+   if (info) {
+      info.addEventListener('change', event => {
+         document.getElementById('networkType').innerHTML = info.type;
+         document.getElementById('effectiveNetworkType').innerHTML = info.effectiveType;
+         document.getElementById('downlinkMax').innerHTML = info.downlinkMax;
+      });
+   }
+
+});
+
