a0ec5312012e69eda731865a513e2e1314dd31b2
Maciej Treder <contact@maciejtreder.com>
2019-12-06 10:59:30 +0100

    event test

--- ambient_light/ambient_light.js
+++ ambient_light/ambient_light.js
@@ -4,39 +4,39 @@ $(document).ready(() => {
 
    $('content').css('background-color', 'rgb(255,255,0)');
 
-   window.addEventListener("devicelight", function (event) {
+   // window.addEventListener("devicelight", function (event) {
 
-      // Read out the lux value
+   //    // Read out the lux value
       
-      var luminosity = event.value;
+   //    var luminosity = event.value;
       
-      // alert(luminosity);
-      details.innerHTML = luminosity;
-      $('content').css('background-color', 'rgba(255,255,0,' + luminosity + ')');
+   //    // alert(luminosity);
+   //    details.innerHTML = luminosity;
+   //    $('content').css('background-color', 'rgba(255,255,0,' + luminosity + ')');
       
       
-   });
+   // });
 
 
 
-   // if (window.AmbientLightSensor){
-   //    try{
-   //    const sensor = new AmbientLightSensor();
-   //    // Detect changes in the light
-   //    sensor.onreading = () => {
-   //       details.innerHTML = sensor.illuminance;
-   //          $('content').css('background-color', 'rgba(255,255,255,' + sensor.illuminance + ')');
-   //    }
+   if (window.AmbientLightSensor){
+      try{
+      const sensor = new AmbientLightSensor();
+      // Detect changes in the light
+      sensor.onreading = () => {
+         details.innerHTML = sensor.illuminance;
+            $('content').css('background-color', 'rgba(255,0,255,' + sensor.illuminance + ')');
+      }
 
-   //    // Has an error occured?
-   //    sensor.onerror = event => document.getElementById("details").innerHTML = event.error.message;
-   //    sensor.start();
-   //    } catch(err) {
-   //    details.innerHTML = err.message;
-   //    }
-   // } else {
-   //    details.innerHTML = 'It looks like your browser doesnt support this feature'; 
-   // }
+      // Has an error occured?
+      sensor.onerror = event => document.getElementById("details").innerHTML = event.error.message;
+      sensor.start();
+      } catch(err) {
+      details.innerHTML = err.message;
+      }
+   } else {
+      details.innerHTML = 'It looks like your browser doesnt support this feature'; 
+   }
 
 });
 
